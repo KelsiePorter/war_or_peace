8d48c2276faac4274827a5f6078afd05d9f34c72
Kelsie Porter <krporter22@gmail.com>
2022-10-09 12:45:23 -0600

    WIP Ceate type and winner method

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,9 +3,30 @@ class Turn
               :player2,
               :spoils_of_war
 
-  def initialize(player1, player2, spoils_of_war)
+  def initialize(player1, player2, spoils_of_war = [])
     @player1 = player1
     @player2 = player2
     @spoils_of_war = spoils_of_war
   end
+
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    else @player1.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2)
+      :mutually_assured_destruction
+    end
+  end
+
+  def winner
+    if type == :basic
+      # return player with higher rank_of_card(0)
+    elsif type == :war
+      # return whichever player has a higher rank_of_card(2)
+    else type == :mutually_assured_destruction
+      "No Winner"
+    end
+  end
+
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,7 +2,43 @@ require 'rspec'
 require './lib/deck'
 require './lib/card'
 require './lib/player'
+require './lib/turn'
 require 'pry'
 
 RSpec.describe Turn do
+  before(:each) do
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, '9', 9)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn = Turn.new(@player1, @player2)
+  end
+  it "create a deck of cards for each player" do
+
+    expect(@turn.player1).to eq(@player1)
+    expect(@turn.player2).to eq(@player2)
+    expect(@turn.spoils_of_war).to eq([])
+  end
+
+  it "each turn has a type" do
+
+    expect(@turn.type).to eq(:basic)
+    expect(@turn.player1.deck.rank_of_card_at(0)).to eq(11)
+    expect(@turn.player2.deck.rank_of_card_at(2)).to eq(12)
+  end
+
+  it "each turn may have a winner" do
+
+
+  end
+
+
 end
