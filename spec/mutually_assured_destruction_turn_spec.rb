376769273ab9009b958b916788edc277cea79e84
Kelsie Porter <krporter22@gmail.com>
2022-10-09 19:16:39 -0600

    Make mutually_assured_destruction_turn_spec tests pass

--- spec/mutually_assured_destruction_turn_spec.rb
+++ spec/mutually_assured_destruction_turn_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe Turn do
     @card3 = Card.new(:heart, '9', 9)
     @card4 = Card.new(:diamond, 'Jack', 11)
     @card5 = Card.new(:heart, '8', 8)
-    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card6 = Card.new(:diamond, '8', 8)
     @card7 = Card.new(:heart, '3', 3)
     @card8 = Card.new(:diamond, '2', 2)
     @deck1 = Deck.new([@card1, @card2, @card5, @card8])
-    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+    @deck2 = Deck.new([@card4, @card3, @card6, @card7])
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
     @turn = Turn.new(@player1, @player2)
@@ -31,32 +31,32 @@ RSpec.describe Turn do
 
   it "each turn has a type" do
 
-    expect(@turn.type).to eq(:basic)
+    expect(@turn.type).to eq(:mutually_assured_destruction)
     expect(@turn.player1.deck.rank_of_card_at(0)).to eq(11)
-    expect(@turn.player2.deck.rank_of_card_at(2)).to eq(12)
+    expect(@turn.player2.deck.rank_of_card_at(2)).to eq(8)
   end
 
   it "each turn may have a winner" do
 
-    expect(@turn.type).to eq(:basic)
-    expect(@turn.winner).to eq(@player1)
+    expect(@turn.type).to eq(:mutually_assured_destruction)
+    expect(@turn.winner).to eq("No Winner")
   end
 
   it "the pile of cards is growing" do
 
     @turn.pile_cards
-    expect(@turn.spoils_of_war).to eq([@card1, @card3])
+    expect(@turn.spoils_of_war).to eq([])
   end
 
-  it "the winner's deck is growing" do
+  it "no award spoils this turn" do
 
-    winner = @turn.winner
     @turn.pile_cards
-    @turn.award_spoils(winner)
     expect(@player1.deck).to eq(@deck1)
     expect(@player2.deck).to eq(@deck2)
-    expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
-    expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
+    expect(@player1.deck.cards).to eq([@card8])
+    expect(@player2.deck.cards).to eq([@card7])
+    expect(@player1.has_lost?).to be false
+    expect(@player2.has_lost?).to be false
   end
 
 
